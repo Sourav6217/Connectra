f20ee6b119be56069bee3bc0379903ffdc2094bc
Sourav Manna <145198861+Sourav6217@users.noreply.github.com>
2026-04-06 00:06:50 +0530

    Move header position to the right

--- styles.py
+++ styles.py
@@ -112,7 +112,7 @@ button[kind="header"] { display: none !important; }
 [data-testid="stHeader"]::after {
   content: "⬡ Connectra • Onchain • Talent Marketplace";
   position: absolute;
-  left: 20px;
+  left: 2000px;
   top: 50%;
   transform: translate(0%, -50%);
   font-family: 'DM Mono', monospace;
