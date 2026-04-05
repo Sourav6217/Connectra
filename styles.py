d2f514918d897dfc1e0aa8ffd968784dacb2588d
Sourav Manna <145198861+Sourav6217@users.noreply.github.com>
2026-04-06 00:05:25 +0530

    Update styles.py

--- styles.py
+++ styles.py
@@ -112,7 +112,7 @@ button[kind="header"] { display: none !important; }
 [data-testid="stHeader"]::after {
   content: "⬡ Connectra • Onchain • Talent Marketplace";
   position: absolute;
-  left: 0%;
+  left: 20px;
   top: 50%;
   transform: translate(0%, -50%);
   font-family: 'DM Mono', monospace;
